3dee3f2a247020bd33cbc3af054943df02b8aec6
flnn <flnnfeng@163.com>
2018-03-06 19:46:20 +0800

    Initial commit

---
+++ src/main/java/com/flnn/spark/kafka/KafkaProperties.java
@@ -0,0 +1,10 @@
+package com.flnn.spark.kafka;
+
+/**
+ * kafka配置文件
+ */
+public class KafkaProperties {
+
+    private static String ZK_ADDRESS = "192.168.1.141:2181,192.168.1.142:2181,192.168.1.143:2181";
+    
+}
